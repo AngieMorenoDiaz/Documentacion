5eb5f8d2cbfcbbd8cf091513bc028f982b683202
Angie Moreno <angievalemd@gmail.com>
2023-04-30 18:16:05 -0500

    Version 6

---
+++ jest.config.js
@@ -0,0 +1,3 @@
+/* eslint-disable semi */
+export const preset = "ts-jest";
+export const testEnvaironment = "node";
---
+++ src/index.test.ts
@@ -0,0 +1,40 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable indent */
+
+//Importar bibliotecas
+
+import Request from "supertest"
+import App from "./Router"
+
+describe (
+    "GET /",
+    ()=>{
+        let app: App
+        beforeAll (
+            ()=>{
+                app=new App()
+                app.start ()
+            }
+        )
+
+        afterAll (
+            ()=>{
+             app.close ()   
+            }
+        )
+
+        test(
+            "Debe devolver un mensaje",
+            async ()=>{
+                const res = await Request (app.app).get("/")
+                expect (res.statusCode).toEqual (200)
+                expect (res.text).toEqual ("Bienvenidos a typescript")
+
+            }
+        )
+
+
+    }
+)
+
