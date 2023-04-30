8d608361c5f06af55547eb65790ce7246acec19d
Angie Moreno <angievalemd@gmail.com>
2023-04-30 15:17:53 -0500

    Version 3

--- swagger/Root.swagger.ts
+++ swagger/Root.swagger.ts
@@ -2,12 +2,12 @@ export default{
 /**
  * @swagger
  * 
- * /;
+ * /:
  *  get:
  *      summary: Obtener un mensaje de prueba
  *      responses:
  *          200: 
  *           description: Has obtenido un mensaje de prueba
- *           type:string
+ *           type: string
  */ 
 }
