1b5f851ce1a5968f4d8b76978ea7b0bac605f4e8
Angie Moreno <angievalemd@gmail.com>
2023-05-31 22:22:36 -0500

    Version 9.2

--- src/App.ts
+++ src/App.ts
@@ -4,21 +4,13 @@
 import swaggerUi from "swagger-ui-express"
 import { swaggerSpec } from "./swagger.conf"
 import express,{Application, Request, Response} from "express"
-import passport from "passport"
 
-import dotenv from "dotenv"
-dotenv.config()
 
 import PacienteRouter from "./routes/Paciente.routes"
 import MedicoRouter from "./routes/medico.routes"
 import FormularioRouter from "./routes/formulario.routes"
 import cors from "cors"
-import miEstrategia from "./config/passport"
 
-import rutas_auth from "./routes/authRoutes"
-
-    
-    
 
 /**
  * @author Angie Valentina Moreno
@@ -29,7 +21,6 @@ class App{
     public app:any
     private server:any
     
-
 
 /**
  * @author Angie Valentina Moreno
@@ -46,21 +37,16 @@ class App{
         )
         this.app.use(cors())
         this.routes()
-        passport.use(miEstrategia)
-        this.app.use(passport.initialize())
     }
+
     /**
      * Definir y agregar las rutas de la API con express
      */
     
-   
-
-    private routes():void{
+       private routes():void {
         this.app.use("/", PacienteRouter)     
         this.app.use("/", MedicoRouter)  
         this.app.use ("/", FormularioRouter)
-        this.app.use("/auth", rutas_auth)
-        this.app.use("/",passport.authenticate("jwt"),PacienteRouter, MedicoRouter, FormularioRouter)
     }
     
     public start():void{
@@ -74,6 +60,6 @@ class App{
 
     }
 
-    
-}
+    }  
+
 export default App
